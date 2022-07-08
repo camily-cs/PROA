69945c8bcf0f8c2377d1d047fd41ea774f22b296
Camily Cruz da Silva <79452420+camily-cs@users.noreply.github.com>
2022-07-08 17:14:19 -0300

    Projeto sobre mim

---
+++ sobreMim_PROA/pags/pagFuturo/script.js
@@ -0,0 +1,13 @@
+var carta = document.getElementById('carta');
+var imgCarta = document.getElementById('img1')
+
+function exibirCarta(){
+    img1.style.cssText = 
+    'margin-left: 20px;'+
+    'margin-top: 20px;'+
+    'width: 100px;'+
+    'height: 100px;'
+
+    carta.style.cssText = 'display:block'
+    
+}
